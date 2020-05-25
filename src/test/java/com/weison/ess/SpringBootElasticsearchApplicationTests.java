ad8f6cdbf98dbd3898ceb91aecbb13314cf0c416
weixiaoxing <weixiaoxing@meishubao.com>
2020-05-25 21:18:40 +0800

    init

---
+++ src/test/java/com/weison/ess/SpringBootElasticsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weison.ess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootElasticsearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
